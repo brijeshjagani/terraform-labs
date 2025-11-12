f5feaa991621c01ba7880a302bd3894a771ef5f2
brijeshjagani <156147315+brijeshjagani@users.noreply.github.com>
2025-11-12 13:01:09 +0530

    Update backend.tf

--- exercise_1/backend.tf
+++ exercise_1/backend.tf
@@ -1,7 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket = "s3tftstemigrate"
-    key    = "s3tstate/backend.tfstate"
-    region = "ap-south-1"
-  }
-}
+bucket = "s3tftstemigrate"
+key    = "s3tstate/backend.tfstate"
+region = "ap-south-1"
